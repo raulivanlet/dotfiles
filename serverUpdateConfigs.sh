8643439b38d4ad29f0edc022aa9ce7ed14acce81
Raul Calin <raulcalinil@gmail.com>
2023-06-06 18:02:42 +0300

    Update

--- serverUpdateConfigs.sh
+++ serverUpdateConfigs.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
+DATA=${HOME}
 
 #--------------------Create homepage folders
-if [ ! -d /home/${USER}/data/homepage ]; then sudo mkdir ${HOME}/data/homepage; fi
-if [ ! -d /home/${USER}/data/homepage/config ]; then sudo mkdir ${HOME}/data/homepage/config; fi
-if [ ! -d /home/${USER}/data/homepage/icons ]; then sudo mkdir ${HOME}/data/homepage/icons; fi
+if [ ! -d /home/${USER}/data/homepage ]; then sudo mkdir ${DATA}/data/homepage; fi
+if [ ! -d /home/${USER}/data/homepage/config ]; then sudo mkdir ${DATA}/data/homepage/config; fi
+if [ ! -d /home/${USER}/data/homepage/icons ]; then sudo mkdir ${DATA}/data/homepage/icons; fi
 
 #--------------------Update homepage files
 if [ -f /home/${USER}/data/homepage/config/bookmarks.yaml ];
-    then sudo bash -c 'cat configs/homepage/bookmarks.yaml > /home/${USER}/data/homepage/config/bookmarks.yaml';
-    else sudo cp configs/homepage/bookmarks.yaml /home/${USER}/data/homepage/config/bookmarks.yaml;
+    then sudo bash -c 'cat configs/homepage/bookmarks.yaml > ${DATA}/data/homepage/config/bookmarks.yaml';
+    else sudo cp configs/homepage/bookmarks.yaml ${DATA}/data/homepage/config/bookmarks.yaml;
 fi
 
 if [ -f /home/${USER}/data/homepage/config/docker.yaml ];
-    then sudo bash -c 'cat configs/homepage/docker.yaml > ${HOME}/data/homepage/config/docker.yaml';
-    else sudo cp configs/homepage/docker.yaml ${HOME}/data/homepage/config/docker.yaml;
+    then sudo bash -c 'cat configs/homepage/docker.yaml > ${DATA}/data/homepage/config/docker.yaml';
+    else sudo cp configs/homepage/docker.yaml ${DATA}/data/homepage/config/docker.yaml;
 fi
 
 if [ -f /home/${USER}/data/homepage/config/services.yaml ];
-    then sudo bash -c 'cat configs/homepage/services.yaml > /home/${USER}/data/homepage/config/services.yaml';
-    else sudo cp configs/homepage/services.yaml /home/${USER}/data/homepage/config/services.yaml;
+    then sudo bash -c 'cat configs/homepage/services.yaml > ${DATA}/data/homepage/config/services.yaml';
+    else sudo cp configs/homepage/services.yaml ${DATA}/data/homepage/config/services.yaml;
 fi
 
 if [ -f /home/${USER}/data/homepage/config/settings.yaml ];
-    then sudo bash -c 'cat configs/homepage/settings.yaml > ${HOME}/data/homepage/config/settings.yaml';
+    then sudo bash -c 'cat configs/homepage/settings.yaml > ${DATA}/data/homepage/config/settings.yaml';
     else sudo cp configs/homepage/settings.yaml ${HOME}/data/homepage/config/settings.yaml;
 fi
 
 if [ -f /home/${USER}/data/homepage/config/widgets.yaml ];
-    then sudo bash -c 'cat configs/homepage/widgets.yaml > /home/${USER}/data/homepage/config/widgets.yaml';
-    else sudo cp configs/homepage/widgets.yaml /home/${USER}/data/homepage/config/widgets.yaml;
+    then sudo bash -c 'cat configs/homepage/widgets.yaml > ${DATA}/data/homepage/config/widgets.yaml';
+    else sudo cp configs/homepage/widgets.yaml ${DATA}/data/homepage/config/widgets.yaml;
 fi
 
 
