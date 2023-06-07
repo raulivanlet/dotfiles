d287ff54e960f35563380e0bfb062ba57b902843
Raul Calin <raulcalinil@gmail.com>
2023-06-07 14:03:10 +0300

    upd

--- serverUpdateConfigs.sh
+++ serverUpdateConfigs.sh
@@ -1,50 +1,49 @@
 #!/bin/bash
 
-DATA=${HOME}
+#--------------------Create DATADIR folder
+DATADIR=${HOME}
+DATADIR=${DATADIR}/servdata
+if [ ! -d  ${DATADIR} ]; then mkdir ${DATADIR}; fi
 
-#--------------------Create homepage folders
-if [ ! -d  ${DATA}/data/homepage ]; then mkdir ${DATA}/data/homepage; fi
-if [ ! -d  ${DATA}/data/homepage/config ]; then mkdir ${DATA}/data/homepage/config; fi
-if [ ! -d  ${DATA}/data/homepage/icons ]; then mkdir ${DATA}/data/homepage/icons; fi
+#--------------------Create homepage folder
+if [ ! -d  ${DATADIR}/homepage ]; then mkdir ${DATADIR}/homepage; fi
 
-#--------------------Update homepage files
-if [ -f  ${DATA}/data/homepage/config/bookmarks.yaml ]; then rm -f ${DATA}/data/homepage/config/bookmarks.yaml; fi
-cp configs/homepage/bookmarks.yaml ${DATA}/data/homepage/config/bookmarks.yaml;
-
-if [ -f  ${DATA}/data/homepage/config/docker.yaml ]; then rm -f ${DATA}/data/homepage/config/docker.yaml; fi
-cp configs/homepage/docker.yaml ${DATA}/data/homepage/config/docker.yaml;
-
-if [ -f  ${DATA}/data/homepage/config/services.yaml ]; then rm -f ${DATA}/data/homepage/config/services.yaml; fi
-cp configs/homepage/services.yaml ${DATA}/data/homepage/config/services.yaml;
-
-if [ -f  ${DATA}/data/homepage/config/settings.yaml ]; then rm -f ${DATA}/data/homepage/config/settings.yaml; fi
-cp configs/homepage/settings.yaml ${HOME}/data/homepage/config/settings.yaml;
-
-if [ -f  ${DATA}/data/homepage/config/widgets.yaml ]; then rm -f ${DATA}/data/homepage/config/widgets.yaml; fi
-cp configs/homepage/widgets.yaml ${DATA}/data/homepage/config/widgets.yaml;
-
+#--------------------Update homepage config files
+HOMECONF=${DATADIR}/homepage/config
+if [ ! -d  ${HOMECONF} ]; then mkdir ${HOMECONF}; fi
+if [ -f  ${HOMECONF}/bookmarks.yaml ]; then rm -f ${HOMECONF}/bookmarks.yaml; fi
+if [ -f  ${HOMECONF}/docker.yaml ]; then rm -f ${HOMECONF}/docker.yaml; fi
+if [ -f  ${HOMECONF}/services.yaml ]; then rm -f ${HOMECONF}/services.yaml; fi
+if [ -f  ${HOMECONF}/settings.yaml ]; then rm -f ${HOMECONF}/settings.yaml; fi
+if [ -f  ${HOMECONF}/widgets.yaml ]; then rm -f ${HOMECONF}/widgets.yaml; fi
+cp configs/homepage/bookmarks.yaml ${HOMECONF}/bookmarks.yaml;
+cp configs/homepage/docker.yaml ${HOMECONF}/docker.yaml;
+cp configs/homepage/services.yaml ${HOMECONF}/services.yaml;
+cp configs/homepage/settings.yaml ${HOMECONF}/settings.yaml;
+cp configs/homepage/widgets.yaml ${HOMECONF}/widgets.yaml;
 
 #--------------------Update homepage icons
-if [ ! -f  ${DATA}/data/homepage/icons/home-assistant.png ]; then cp images/home-assistant.png ${DATA}/data/homepage/icons/home-assistant.png; fi
-if [ ! -f  ${DATA}/data/homepage/icons/jellyfin.png ]; then cp images/jellyfin.png ${DATA}/data/homepage/icons/jellyfin.png; fi
-if [ ! -f  ${DATA}/data/homepage/icons/mainsail.png ]; then cp images/mainsail.png ${DATA}/data/homepage/icons/mainsail.png; fi
-if [ ! -f  ${DATA}/data/homepage/icons/nextcloud.png ]; then cp images/nextcloud.png ${DATA}/data/homepage/icons/nextcloud.png; fi
-if [ ! -f  ${DATA}/data/homepage/icons/proxmox.png ]; then cp images/proxmox.png ${DATA}/data/homepage/icons/proxmox.png; fi
-if [ ! -f  ${DATA}/data/homepage/icons/qbittorrent.png ]; then cp images/qbittorrent.png ${DATA}/data/homepage/icons/qbittorrent.png; fi
-if [ ! -f  ${DATA}/data/homepage/icons/syncthing.png ]; then cp images/syncthing.png ${DATA}/data/homepage/icons/syncthing.png; fi
-if [ ! -f  ${DATA}/data/homepage/icons/zigbee2mqtt.png ]; then cp images/zigbee2mqtt.png ${DATA}/data/homepage/icons/zigbee2mqtt.png; fi
-
+HOMEICO=${DATADIR}/homepage/icons
+if [ ! -d  ${HOMEICO} ]; then mkdir ${HOMEICO}; fi
+if [ ! -f  ${HOMEICO}/home-assistant.png ]; then cp images/home-assistant.png ${HOMEICO}/home-assistant.png; fi
+if [ ! -f  ${HOMEICO}/jellyfin.png ]; then cp images/jellyfin.png ${HOMEICO}/jellyfin.png; fi
+#if [ ! -f  ${HOMEICO}/mainsail.png ]; then cp images/mainsail.png ${HOMEICO}/mainsail.png; fi
+if [ ! -f  ${HOMEICO}/nextcloud.png ]; then cp images/nextcloud.png ${HOMEICO}/nextcloud.png; fi
+if [ ! -f  ${HOMEICO}/proxmox.png ]; then cp images/proxmox.png ${HOMEICO}/proxmox.png; fi
+#if [ ! -f  ${HOMEICO}/qbittorrent.png ]; then cp images/qbittorrent.png ${HOMEICO}/qbittorrent.png; fi
+if [ ! -f  ${HOMEICO}/syncthing.png ]; then cp images/syncthing.png ${HOMEICO}/syncthing.png; fi
+if [ ! -f  ${HOMEICO}/zigbee2mqtt.png ]; then cp images/zigbee2mqtt.png ${HOMEICO}/zigbee2mqtt.png; fi
 
 #--------------------Create zigbee2mqtt folders
-if [ ! -d  ${DATA}/data/zigbee2mqtt ]; then sudo mkdir /home/${USER}/data/zigbee2mqtt; fi
-if [ ! -d  ${DATA}/data/zigbee2mqtt/zigbee ]; then sudo mkdir /home/${USER}/data/zigbee2mqtt/zigbee; fi
-if [ ! -d  ${DATA}/data/zigbee2mqtt/mosquitto ]; then sudo mkdir /home/${USER}/data/zigbee2mqtt/mosquitto; fi
+if [ ! -d  ${DATADIR}/zigbee2mqtt ]; then mkdir ${DATADIR}/zigbee2mqtt; fi
+if [ ! -d  ${DATADIR}/zigbee2mqtt/zigbee ]; then mkdir ${DATADIR}/zigbee2mqtt/zigbee; fi
+if [ ! -d  ${DATADIR}/zigbee2mqtt/mosquitto ]; then mkdir ${DATADIR}/zigbee2mqtt/mosquitto; fi
 
 #--------------------Update zigbee2mqtt files
-if [ -f  ${DATA}/data/zigbee2mqtt/zigbee/config.yaml ]; then rm -f ${DATA}/data/zigbee2mqtt/zigbee/config.yaml; fi
-cp configs/server/zigbeeconfig.yaml ${DATA}/data/zigbee2mqtt/zigbee/config.yaml;
+if [ -f  ${DATADIR}/zigbee2mqtt/zigbee/config.yaml ]; then rm -f ${DATADIR}/zigbee2mqtt/zigbee/config.yaml; fi
+cp configs/server/zigbeeconfig.yaml ${DATADIR}/zigbee2mqtt/zigbee/config.yaml;
 
 #--------------------Update mqtt files
-if [ -f  ${DATA}/data/zigbee2mqtt/mosquitto/mosquitto.conf ]; then rm -f ${DATA}/data/zigbee2mqtt/mosquitto/mosquitto.conf; fi
-cp configs/server/mosquitto.conf ${DATA}/data/zigbee2mqtt/mosquitto/mosquitto.conf;
+if [ -f  ${DATADIR}/zigbee2mqtt/mosquitto/mosquitto.conf ]; then rm -f ${DATADIR}/zigbee2mqtt/mosquitto/mosquitto.conf; fi
+cp configs/server/mosquitto.conf ${DATADIR}/zigbee2mqtt/mosquitto/mosquitto.conf;
 
